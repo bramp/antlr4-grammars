300c07366dbba9276ff8d1473616cc999a37a778
Andrew Brampton <bramp@google.com>
2017-10-07 10:53:49 -0700

    Add Apache License and copyright notices.

--- README.md
+++ README.md
@@ -210,5 +210,25 @@ git submodule update
 # Run make and all the magic will happen
 make all
 
-# Update the table in the README.me with the output
+# Update the table in the README.md with the output
+```
+
+## Licence (Apache 2)
+
+*This is not an official Google product (experimental or otherwise), it is just code that happens to be owned by Google.*
+
+```
+Copyright 2017 Google Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 ```
--- maketest.go
+++ maketest.go
@@ -1,3 +1,17 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // maketest creates the test and example files for the given grammar.
 package main
 
@@ -17,7 +31,21 @@ const GRAMMARS_ROOT = "grammars-v4"
 
 // TESTFILE is the template for a go test file for this grammar.
 // It expects to be executed with a pom.
-const TESTFILE = `// Package {{ .Name }}_test contains tests for the {{ .LongName }} grammar.
+const TESTFILE = `// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package {{ .Name }}_test contains tests for the {{ .LongName }} grammar.
 // The tests should be run with the -timeout flag, to ensure the parser doesn't
 // get stuck.
 //
